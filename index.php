d44e0742e153abfbf29a2fc0d4c48c3171bfeaa2
eryo05 <jeremy.gauthier@et.simplon-roanne.com>
2018-07-23 12:31:25 +0200

    exo1

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$name = "jeremy";
+echo $name;
+$lastname = " gauthier";
+$firstname = " eryo";
+$age = 54;
+echo $firstname . $lastname . " " . $age;
+$km = 1;
+echo " ". $km;
+$km = 3;
+echo " ". $km;
+$km = 125;
+echo " ". $km;
+
+$ville = "roanne";
+$numero = 15;
+$virgule = 15.56;
+$vrai = true;
+
+echo " " . $ville . " " . $numero . " " . $virgule . " " . $vrai;
+$numero2 = null . "<br>";
+echo $numero2;
+$numero2 = 77;
+echo $numero2 . "<br>";
+echo "Bonjour " . $name . ", comment vas tu ? <br>";
+echo "Bonjour" . $lastname . $firstname . ",tu as " . $age . " ans <br>";
+$res = 3 + 4;
+$res2 = 5 * 20 ;
+$res3 = 45 / 5;
+echo "<br>" . $res . " " . $res2 . " " . $res3;
+
+?>
